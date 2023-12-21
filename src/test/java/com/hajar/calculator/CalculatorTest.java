f21aadfceff3043b684b1a4df8b68ce6b8e14ee3
hajar0123 <hajarsniba2001@gmail.com>
2023-12-21 11:16:27 -0500

    acceptance

---
+++ src/test/java/com/hajar/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.hajar.calculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+private Calculator calculator = new Calculator();
+
+@Test
+public void testSum() {
+assertEquals(8, calculator.sum(3, 5));
+}
+}
